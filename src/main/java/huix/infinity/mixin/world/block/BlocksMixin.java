6d2f4da9b4973ab4aeb019a2770746d0a7cfe418
limingzxc <2904962732@qq.com>
2025-01-25 23:28:57 +0800

    set more blocks Hardness

--- src/main/java/huix/infinity/mixin/world/block/BlocksMixin.java
+++ src/main/java/huix/infinity/mixin/world/block/BlocksMixin.java
@@ -5,8 +5,6 @@ import net.minecraft.util.ColorRGBA;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ColoredFallingBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.MapColor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -16,32 +14,62 @@ public class BlocksMixin {
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;strength(FF)Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
             , ordinal = 0), method = "<clinit>")
-    private static BlockBehaviour.Properties ifw_stoneHardness(BlockBehaviour.Properties instance, float destroyTime, float explosionResistance) {
-        return BlockBehaviour.Properties.of().mapColor(MapColor.STONE).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2.4F, 6.0F);
+    private static BlockBehaviour.Properties ifw_stoneHardness(BlockBehaviour.Properties properties, float destroyTime, float explosionResistance) {
+        return properties.strength(2.4F, 6.0F);
     }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;strength(FF)Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
             , ordinal = 33), method = "<clinit>")
-    private static BlockBehaviour.Properties ifw_obsidianHardness(BlockBehaviour.Properties instance, float destroyTime, float explosionResistance) {
-        return BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2.4F, 1200.0F);
+    private static BlockBehaviour.Properties ifw_obsidianHardness(BlockBehaviour.Properties properties, float destroyTime, float explosionResistance) {
+        return properties.strength(2.4F, 1200.0F);
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;instabreak()Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
+            , ordinal = 8), method = "<clinit>")
+    private static BlockBehaviour.Properties ifw_shortGrassHardness(BlockBehaviour.Properties properties) {
+        return properties.strength(0.1F);
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;instabreak()Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
+            , ordinal = 54), method = "<clinit>")
+    private static BlockBehaviour.Properties ifw_tallGrassHardness(BlockBehaviour.Properties properties) {
+        return properties.strength(0.1F);
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;instabreak()Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
+            , ordinal = 9), method = "<clinit>")
+    private static BlockBehaviour.Properties ifw_fernHardness(BlockBehaviour.Properties properties) {
+        return properties.strength(0.1F);
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;instabreak()Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
+            , ordinal = 55), method = "<clinit>")
+    private static BlockBehaviour.Properties ifw_largeFernHardness(BlockBehaviour.Properties properties) {
+        return properties.strength(0.1F);
     }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;strength(FF)Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
             , ordinal = 172), method = "<clinit>")
-    private static BlockBehaviour.Properties ifw_cryingObsidianHardness(BlockBehaviour.Properties instance, float destroyTime, float explosionResistance) {
-        return BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2.4F, 1200.0F).lightLevel((p_152651_) -> 10);
+    private static BlockBehaviour.Properties ifw_cryingObsidianHardness(BlockBehaviour.Properties properties, float destroyTime, float explosionResistance) {
+        return properties.strength(2.4F, 1200.0F);
     }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;strength(F)Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;")
             , method = "log(Lnet/minecraft/world/level/material/MapColor;Lnet/minecraft/world/level/material/MapColor;)Lnet/minecraft/world/level/block/Block;")
-    private static BlockBehaviour.Properties ifw_logWithTool_0(BlockBehaviour.Properties instance, float strength) {
-        return instance.strength(1.2F).requiresCorrectToolForDrops();
+    private static BlockBehaviour.Properties ifw_logWithTool_0(BlockBehaviour.Properties properties, float strength) {
+        return properties.strength(1.2F).requiresCorrectToolForDrops();
     }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;strength(F)Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;")
             , method = "log(Lnet/minecraft/world/level/material/MapColor;Lnet/minecraft/world/level/material/MapColor;Lnet/minecraft/world/level/block/SoundType;)Lnet/minecraft/world/level/block/Block;")
-    private static BlockBehaviour.Properties ifw_logWithTool_1(BlockBehaviour.Properties instance, float strength) {
-        return instance.strength(1.2F).requiresCorrectToolForDrops();
+    private static BlockBehaviour.Properties ifw_logWithTool_1(BlockBehaviour.Properties properties, float strength) {
+        return properties.strength(1.2F).requiresCorrectToolForDrops();
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;strength(F)Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;")
+            , method = "leaves")
+    private static BlockBehaviour.Properties ifw_leavesHardness(BlockBehaviour.Properties properties, float strength) {
+        return properties.strength(0.6F);
     }
 
     @Redirect(at = @At(value = "NEW", target = "(Lnet/minecraft/util/ColorRGBA;Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;)Lnet/minecraft/world/level/block/ColoredFallingBlock;"
