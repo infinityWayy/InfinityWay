4c5b18e92fbbf403b820655d311dd94428a1b677
Huix <huix@huix.fun>
2025-02-01 22:01:38 +0800

    Remove shore gravels

---
+++ src/main/java/huix/infinity/mixin/data/worldgen/features/MiscOverworldFeaturesMixin.java
@@ -0,0 +1,16 @@
+package huix.infinity.mixin.data.worldgen.features;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.features.MiscOverworldFeatures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MiscOverworldFeatures.class)
+public class MiscOverworldFeaturesMixin {
+
+}
