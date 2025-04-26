318c2a7f82e01d3642a0b52049514af4dc45a8c3
limingzxc <2904962732@qq.com>
2025-04-27 01:15:10 +0800

    fix

--- src/main/java/huix/infinity/extension/func/ItemExtension.java
+++ src/main/java/huix/infinity/extension/func/ItemExtension.java
@@ -11,13 +11,15 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.common.Tags;
 import org.jetbrains.annotations.ApiStatus;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public interface ItemExtension {
 
     default float getReachBonus() {
         Holder<Item> holder = getItemHolder();
-        if (holder.is(Tags.Items.MELEE_WEAPON_TOOLS)) {
+        if (!Objects.requireNonNull(holder.getKey()).location().getNamespace().equals("ifw")
+                && holder.is(Tags.Items.TOOLS)) {
             return 0.75F;
         }
         return 0.0F;
@@ -32,7 +34,7 @@ public interface ItemExtension {
         Item instance = (Item) this;
         if (instance.components().has(DataComponents.MAX_DAMAGE)) return;
         DataComponentMap.Builder builder = DataComponentMap.builder().addAll(instance.components);
-        patch.entrySet().forEach((entry) -> builder.set((DataComponentType)entry.getKey(), ((Optional)entry.getValue()).orElse((Object)null)));
+        patch.entrySet().forEach((entry) -> builder.set((DataComponentType)entry.getKey(), ((Optional)entry.getValue()).orElse(null)));
         instance.components = Item.Properties.COMPONENT_INTERNER.intern(Item.Properties.validateComponents(builder.build()));
     }
 
@@ -40,7 +42,7 @@ public interface ItemExtension {
     default void ifw_modifyDefaultComponentsToAnvil(DataComponentPatch patch) {
         Item instance = (Item) this;
         DataComponentMap.Builder builder = DataComponentMap.builder().addAll(instance.components);
-        patch.entrySet().forEach((entry) -> builder.set((DataComponentType)entry.getKey(), ((Optional)entry.getValue()).orElse((Object)null)));
+        patch.entrySet().forEach((entry) -> builder.set((DataComponentType)entry.getKey(), ((Optional)entry.getValue()).orElse(null)));
         instance.components = Item.Properties.COMPONENT_INTERNER.intern(Item.Properties.validateComponents(builder.build()));
     }
 
