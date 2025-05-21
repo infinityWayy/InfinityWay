4abd7fe5f9448da882953f44be9a6376c4b97b65
Inf1nity <1449896523@qq.com>
2025-05-22 01:06:25 +0800

    Added MITE Enchantments

--- src/main/java/huix/infinity/common/enchantment/IFWEnchantments.java
+++ src/main/java/huix/infinity/common/enchantment/IFWEnchantments.java
@@ -19,31 +19,31 @@ import net.minecraft.world.item.enchantment.effects.EnchantmentAttributeEffect;
 
 public class IFWEnchantments {
     //Common
-    private static final ResourceLocation ifw_unbreaking = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "unbreaking");
+//    private static final ResourceLocation ifw_unbreaking = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "unbreaking");
     //Weapon
-    private static final ResourceLocation ifw_disarming = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "disarming");
-    private static final ResourceLocation ifw_quickness = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "quickness");
-    private static final ResourceLocation ifw_accuracy = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "accuracy");
-    private static final ResourceLocation ifw_poison = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "poison");
-    private static final ResourceLocation ifw_butchering = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "butchering");
-    private static final ResourceLocation ifw_stun = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "stun");
-    private static final ResourceLocation ifw_vampiric = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "vampiric");
-    private static final ResourceLocation ifw_recovery = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "recovery");
-    private static final ResourceLocation ifw_slaying = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "slaying");
-    private static final ResourceLocation ifw_cleaving = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "cleaving");
+//    private static final ResourceLocation ifw_disarming = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "disarming");
+//    private static final ResourceLocation ifw_quickness = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "quickness");
+//    private static final ResourceLocation ifw_accuracy = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "accuracy");
+//    private static final ResourceLocation ifw_poison = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "poison");
+//    private static final ResourceLocation ifw_butchering = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "butchering");
+//    private static final ResourceLocation ifw_stun = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "stun");
+//    private static final ResourceLocation ifw_vampiric = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "vampiric");
+//    private static final ResourceLocation ifw_recovery = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "recovery");
+//    private static final ResourceLocation ifw_slaying = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "slaying");
+//    private static final ResourceLocation ifw_cleaving = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "cleaving");
     //Tool
-    private static final ResourceLocation ifw_harvesting = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "harvesting");
-    private static final ResourceLocation ifw_piercing = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "piercing");
-    private static final ResourceLocation ifw_luring = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "luring");
-    private static final ResourceLocation ifw_fertility = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "fertility");
-    private static final ResourceLocation ifw_treeFelling = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "treeFelling");
-    private static final ResourceLocation ifw_fortune = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "fortune");
+//    private static final ResourceLocation ifw_harvesting = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "harvesting");
+//    private static final ResourceLocation ifw_piercing = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "piercing");
+//    private static final ResourceLocation ifw_luring = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "luring");
+//    private static final ResourceLocation ifw_fertility = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "fertility");
+//    private static final ResourceLocation ifw_treeFelling = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "treeFelling");
+//    private static final ResourceLocation ifw_fortune = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "fortune");
     //Armor
-    private static final ResourceLocation ifw_freeAction = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "freeAction");
-    private static final ResourceLocation ifw_regeneration = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "regeneration");
-    private static final ResourceLocation ifw_speed = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "speed");
-    private static final ResourceLocation ifw_endurance = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "endurance");
-    private static final ResourceLocation ifw_protection = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "protection");
+//    private static final ResourceLocation ifw_freeAction = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "freeAction");
+//    private static final ResourceLocation ifw_regeneration = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "regeneration");
+      private static final ResourceLocation ifw_speed = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "speed");
+//    private static final ResourceLocation ifw_endurance = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "endurance");
+//    private static final ResourceLocation ifw_protection = ResourceLocation.fromNamespaceAndPath(InfinityWay.MOD_ID, "protection");
 
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder().add(
             Registries.ENCHANTMENT, bootstrap -> {
