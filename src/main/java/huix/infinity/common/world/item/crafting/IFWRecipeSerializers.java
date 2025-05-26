3ff84f1fe969b6eb6d6e9f333edbe2c5da57d848
Inf1nity <1449896523@qq.com>
2025-05-26 23:08:56 +0800

    Revert "Initial commit"
    This reverts commit 9fe2ae7fc3fb1f7905174c8c16a7368c2f325328.

--- src/main/java/huix/infinity/common/world/item/crafting/IFWRecipeSerializers.java
+++ src/main/java/huix/infinity/common/world/item/crafting/IFWRecipeSerializers.java
@@ -1,7 +1,5 @@
 package huix.infinity.common.world.item.crafting;
 
-import huix.infinity.common.world.item.crafting.mite.MITERecipe;
-import huix.infinity.common.world.item.crafting.mite.MITERecipeSerializer;
 import huix.infinity.init.InfinityWay;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.crafting.RecipeSerializer;
@@ -12,7 +10,4 @@ public class IFWRecipeSerializers {
 
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(BuiltInRegistries.RECIPE_SERIALIZER, InfinityWay.MOD_ID);
 
-    // 添加MITE配方序列化器
-    public static final DeferredHolder<RecipeSerializer<?>, RecipeSerializer<MITERecipe>> MITE_CRAFTING =
-            RECIPE_SERIALIZERS.register("mite_crafting", () -> MITERecipeSerializer.INSTANCE);
 }
--- src/main/java/huix/infinity/common/world/item/crafting/IFWRecipeTypes.java
+++ src/main/java/huix/infinity/common/world/item/crafting/IFWRecipeTypes.java
@@ -1,9 +1,9 @@
 package huix.infinity.common.world.item.crafting;
 
-import huix.infinity.common.world.item.crafting.mite.MITERecipe;
-import huix.infinity.common.world.item.crafting.mite.MITERecipeType;
 import huix.infinity.init.InfinityWay;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -12,7 +12,4 @@ public class IFWRecipeTypes {
 
     public static final DeferredRegister<RecipeType<?>> RECIPES = DeferredRegister.create(BuiltInRegistries.RECIPE_TYPE, InfinityWay.MOD_ID);
 
-    // 添加MITE配方类型
-    public static final DeferredHolder<RecipeType<?>, RecipeType<MITERecipe>> MITE_CRAFTING =
-            RECIPES.register("mite_crafting", () -> MITERecipeType.MITE_CRAFTING);
 }
