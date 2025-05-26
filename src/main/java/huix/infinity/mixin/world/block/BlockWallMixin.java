3de66bb86fc27c44e6f28cfdd5ad45ba3dd6c553
Inf1nity <1449896523@qq.com>
2025-05-26 16:22:48 +0800

    调整了墙和栅栏对玩家的碰撞箱

---
+++ src/main/java/huix/infinity/mixin/world/block/BlockWallMixin.java
@@ -0,0 +1,32 @@
+package huix.infinity.mixin.world.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.WallBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WallBlock.class)
+public class BlockWallMixin {
+
+    @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
+    private void makeClimbableForPlayer(BlockState state, BlockGetter level, BlockPos pos,
+                                        CollisionContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        if (context instanceof EntityCollisionContext entityContext) {
+            Entity entity = entityContext.getEntity();
+            if (entity instanceof Player) {
+                VoxelShape climbableShape = Shapes.box(0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+                cir.setReturnValue(climbableShape);
+            }
+        }
+    }
+}
