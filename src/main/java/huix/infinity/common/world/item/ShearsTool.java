8ff3780a975c772c1c0b5e01f98e7f940fd97f58
Inf1nity <1449896523@qq.com>
2025-05-30 02:26:26 +0800

    修复了剪刀不能剪羊的BUG

--- src/main/java/huix/infinity/common/world/item/ShearsTool.java
+++ src/main/java/huix/infinity/common/world/item/ShearsTool.java
@@ -10,9 +10,11 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Shearable;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.Tool;
@@ -68,7 +70,8 @@ public class ShearsTool extends IFWTieredItem {
         Player player = context.getPlayer();
         ItemStack itemstack = context.getItemInHand();
         if (Screen.hasControlDown() && block instanceof GrowingPlantHeadBlock growingplantheadblock && !growingplantheadblock.isMaxAge(blockstate)) {
-            if (player instanceof ServerPlayer) CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer)player, blockpos, itemstack);
+            if (player instanceof ServerPlayer)
+                CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, blockpos, itemstack);
             level.playSound(player, blockpos, SoundEvents.GROWING_PLANT_CROP, SoundSource.BLOCKS, 1.0F, 1.0F);
             BlockState blockstate1 = growingplantheadblock.getMaxAgeState(blockstate);
             level.setBlockAndUpdate(blockpos, blockstate1);
@@ -87,6 +90,18 @@ public class ShearsTool extends IFWTieredItem {
             return InteractionResult.SUCCESS;
         } else
 
-        return super.useOn(context);
+            return super.useOn(context);
     }
-}
+        public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity entity, InteractionHand hand) {
+            if (entity instanceof Shearable shearable && shearable.readyForShearing()) {
+                if (!player.level().isClientSide) {
+                    shearable.shear(SoundSource.PLAYERS);
+                    stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
+                    return InteractionResult.SUCCESS;
+                }
+                return InteractionResult.sidedSuccess(player.level().isClientSide);
+            }
+            return InteractionResult.PASS;
+        }
+
+    }
